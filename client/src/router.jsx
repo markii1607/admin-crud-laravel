17eaf6252606da644bdbb2d9a3a56162305ff688
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-16 23:26:02 +0800

    Add additional route and views

--- client/src/router.jsx
+++ client/src/router.jsx
@@ -3,19 +3,38 @@ import Login from './views/Login'
 import Signup from './views/Signup'
 import Users from './views/Users'
 import NotFound from './views/NotFound'
+import Dashboard from './views/Dashboard'
+import DefaultLayout from './components/DefaultLayout'
+import GuestLayout from './components/GuestLayout'
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login />
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+        ]
     },
     {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
     },
     {
         path: '*',
