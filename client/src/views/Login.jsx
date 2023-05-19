f9404c78c626ffae14592100191e08377430c6e3
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-19 22:27:40 +0800

    Create the signup form

--- client/src/views/Login.jsx
+++ client/src/views/Login.jsx
@@ -10,6 +10,7 @@ function Login() {
     <div className="login-signup-form animated fadeInDown">
       <div className="form">
         <form onSubmit={onSubmit}>
+          <h1 className="title">Login into your account</h1>
           <input type="email" placeholder="Email" />
           <input type="password" placeholder="Password" />
           <button className="btn btn-block">Login</button>
