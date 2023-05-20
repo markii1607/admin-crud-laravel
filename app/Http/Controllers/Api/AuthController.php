74e8df089cad2e5e1ba62150ade9c01878ec7fd0
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-20 22:25:13 +0800

    Create login logic

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -7,12 +7,25 @@ use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
     public function login(LoginRequest $request)
     {
-        
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Provided email address or password is incorrect'
+            ]);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
     }
 
     public function signup(SignupRequest $request)
@@ -31,6 +44,6 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
-
+        
     }
 }
