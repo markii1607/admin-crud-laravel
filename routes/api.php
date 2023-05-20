555677b0b8cce6c58ab7c84436f13ed70215b482
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-20 22:21:12 +0800

    Create routes for API calls

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller('/signup', [AuthController::class, 'signup']);
+Route::controller('/login', [AuthController::class, 'login']);
+Route::controller('/logout', [AuthController::class, 'logout']);
