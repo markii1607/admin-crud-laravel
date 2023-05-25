18b31ac4421cfdd439eb18e703e61c933e883ee5
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-25 23:09:11 +0800

    Fetch user data from login

--- client/src/components/DefaultLayout.jsx
+++ client/src/components/DefaultLayout.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Link, Navigate, Outlet } from 'react-router-dom'
 import { useStateContext } from '../contexts/ContextProvider'
+import axiosClient from '../axios-client'
 
 function DefaultLayout() {
-    const { user, token } = useStateContext()
+    const { user, token, setUser } = useStateContext()
 
     // redirect the user if not authenticated
     if (!token) {
@@ -13,6 +14,14 @@ function DefaultLayout() {
     const onLogout = (e) => {
         e.preventDefault()
     }
+
+    // fetch user data
+    useEffect(() => {
+        axiosClient.get('/user')
+            .then(({ data }) => {
+                setUser(data)
+            })
+    }, [])
 
     return (
         <div id="defaultLayout">
