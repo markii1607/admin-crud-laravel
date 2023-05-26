d16d39866b39ab931ad5d097a2e2ca637dc13654
Mark Jerly Bundalian <mbundalian@fullscale.io>
2023-05-26 22:13:30 +0800

    Add design and implement user delete

--- client/src/views/Users.jsx
+++ client/src/views/Users.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import axiosClient from '../axios-client'
+import { Link } from 'react-router-dom'
 
 function Users() {
   const [users, setUsers] = useState([])
@@ -15,15 +16,70 @@ function Users() {
       .then(({ data }) => {
         setLoading(false)
         console.log(data)
+        setUsers(data.data)
       })
       .catch(() => {
         setLoading(false)
       })
   }
 
+  const onDelete = (u) => {
+    if (!window.confirm("Are you sure you want to delete this user?"))
+      return
+    
+    axiosClient.delete(`/users/${u.id}`)
+      .then(() => {
+        // TODO show notif
+        getUsers()
+      })
+  }
+
   return (
     <div>
-      Users
+      <div style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
+        <h1>Users</h1>
+
+        <Link to="/users/new" className="btn-add">Add new</Link>
+      </div>
+
+      <div className="card animated fadeInDown">
+        <table>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Create Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          {loading && (
+            <tbody>
+              <tr>
+                <td colSpan="5" className="text-center">
+                  Loading...
+                </td>
+              </tr>
+            </tbody>
+          )}
+          {!loading && (
+            <tbody>
+              {users.map(u => (
+                <tr key={u.id}>
+                  <td>{u.id}</td>
+                  <td>{u.name}</td>
+                  <td>{u.email}</td>
+                  <td>{u.created_at}</td>
+                  <td>
+                    <Link className="btn-edit" to={'/users/'+u.id}>Edit</Link>&nbsp;
+                    <button onClick={ev => onDelete(u)} className="btn-delete">Delete</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          )}
+        </table>
+      </div>
     </div>
   )
 }
